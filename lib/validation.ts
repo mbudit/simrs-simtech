9e8a0a7642cd4c0d9bc9601b4ba8e4d30e86da49
M. Budi Triono <114796020+mbudit@users.noreply.github.com>
2024-11-12 16:53:50 +0700

    initial commit

---
+++ lib/validation.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const UserFormValidation = z.object({
+    name: z.string()
+    .min(8,"Username must be at least 8 characters.")
+    .max(50,"Username has exceeded 50 characters."),
+    email: z.string().email("Invalid email address."),
+    phone: z.string().refine((phone) => /^\+?[1-9]\d{1,14}$/.test(phone), 'Invalid phone number.')
+  })
